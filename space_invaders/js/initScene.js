39567152fb32f13880d9d3cc8c31982de50fc7f0
Edward Januszewski <e.januszewski@gmail.com>
2015-09-30 14:01:12 +0100

    Space invaders start

---
+++ space_invaders/js/initScene.js
@@ -0,0 +1,68 @@
+﻿///<reference path="http://cdn.babylonjs.com/2-2/babylon.max.js" />
+
+// Global variables
+var canvas, engine, scene, camera, score = 0;
+var TOAD_MODEL;
+var player;
+
+// An array to store each ending of the lane
+var ENDINGS = [];
+
+/**
+* Load the scene when the canvas is fully loaded
+*/
+document.addEventListener("DOMContentLoaded", function () {
+    if (BABYLON.Engine.isSupported()) {
+        initScene();
+    }
+}, false);
+
+/**
+ * Creates a new BABYLON Engine and initialize the scene
+ */
+function initScene() {
+    // Get canvas
+    canvas = document.getElementById("renderCanvas");
+
+    // Create babylon engine
+    engine = new BABYLON.Engine(canvas, true);
+
+    // Create scene
+    scene = new BABYLON.Scene(engine);
+
+    // Create the camera
+    camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 10, -30), scene);
+    camera.setTarget(new BABYLON.Vector3(0, 0, 10));    
+    camera.attachControl(canvas);
+
+    var ground = new BABYLON.Mesh.CreateGround("ground", 50, 50, 0, scene);
+
+
+    // Create light
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 10, 0), scene);
+    light.intensity = 0.7;
+
+    initGame();
+
+    engine.runRenderLoop(function () {
+        scene.render();
+        player.position.x = camera.position.x;
+    })
+}
+
+/**
+ * Initialize the game
+ */
+function initGame() {
+
+    player = BABYLON.Mesh.CreateBox("player", { width: 2, height: 1, depth: 2}, scene, true);
+    player.position.y = 1;
+    camera.setTarget(scene.getMeshByName("player").position);
+
+    var playerMaterial = new BABYLON.StandardMaterial("playerMat", scene);
+    playerMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0); //Red
+
+    player.setMaterialByID("playerMat");
+
+
+}
---
+++ toad_attack/js/initScene.js
@@ -0,0 +1,62 @@
+﻿///<reference path="http://cdn.babylonjs.com/2-2/babylon.max.js" />
+
+// Global variables
+var canvas, engine, scene, camera, score = 0;
+var TOAD_MODEL;
+
+// An array to store each ending of the lane
+var ENDINGS = [];
+
+/**
+* Load the scene when the canvas is fully loaded
+*/
+document.addEventListener("DOMContentLoaded", function () {
+    if (BABYLON.Engine.isSupported()) {
+        initScene();
+    }
+}, false);
+
+/**
+ * Creates a new BABYLON Engine and initialize the scene
+ */
+function initScene() {
+    // Get canvas
+    canvas = document.getElementById("renderCanvas");
+
+    // Create babylon engine
+    engine = new BABYLON.Engine(canvas, true);
+
+    // Create scene
+    scene = new BABYLON.Scene(engine);
+
+    // Create the camera
+    camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 10, -30), scene);
+    camera.setTarget(new BABYLON.Vector3(0, 0, 10));
+    camera.attachControl(canvas);
+
+    // Create light
+    var light = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 5, -5), scene);
+
+    initGame();
+
+    engine.runRenderLoop(function () {
+        scene.render();
+    })
+}
+
+/**
+ * Initialize the game
+ */
+function initGame() {
+
+    BABYLON.Mesh.CreatePlane("front", 10, scene).scaling.y = 2;
+
+    /*BABYLON.Mesh.CreateSphere("sphere", 10, 1, scene);
+    BABYLON.Mesh.CreateLines("lines", [
+        new BABYLON.Vector3(0, 10, 0),
+        new BABYLON.Vector3(10, 20, 0)
+    ], scene);
+    BABYLON.Mesh.CreateDashedLines("dashedLines", [
+        new BABYLON.Vector3(10, 10, 0),
+        new BABYLON.Vector3(20, 30, 0)], 1, 2, 50, scene);*/
+}
