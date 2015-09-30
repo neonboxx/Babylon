78e3ce0be75b913a2d2a13de04d2dc6d568e5b64
Edward Januszewski <e.januszewski@gmail.com>
2015-10-01 00:24:10 +0100

    Start of bullets and move player.

--- space_invaders/js/initScene.js
+++ space_invaders/js/initScene.js
@@ -7,6 +7,9 @@ var player;
 
 // An array to store each ending of the lane
 var ENDINGS = [];
+var keys = { left: 0, right: 0, space: 0 };
+var shot;
+var shots = [];
 
 /**
 * Load the scene when the canvas is fully loaded
@@ -16,6 +19,33 @@ document.addEventListener("DOMContentLoaded", function () {
         initScene();
     }
 }, false);
+
+window.addEventListener("keydown", handleKeyDown, false);
+window.addEventListener("keyup", handleKeyUp, false);
+
+function handleKeyDown(evt) {
+    if (evt.keyCode === 37) {//Left
+        keys.left = 1;
+    }
+    if (evt.keyCode === 39) {//Right
+        keys.right = 1;
+    }
+    if (evt.keyCode === 32) {//Space
+        keys.space = 1;
+    }
+}
+
+function handleKeyUp(evt) {
+    if (evt.keyCode === 37) {
+        keys.left = 0;
+    }
+    if (evt.keyCode === 39) {
+        keys.right = 0;
+    }
+    if (evt.keyCode === 32) {//Space
+        keys.space = 0;
+    }
+}
 
 /**
  * Creates a new BABYLON Engine and initialize the scene
@@ -29,14 +59,14 @@ function initScene() {
 
     // Create scene
     scene = new BABYLON.Scene(engine);
+    scene.debugLayer.show();
 
     // Create the camera
-    camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 10, -30), scene);
-    camera.setTarget(new BABYLON.Vector3(0, 0, 10));    
+    camera = new BABYLON.FollowCamera("camera", new BABYLON.Vector3(0, 20, -60), scene);
+    camera.setTarget(new BABYLON.Vector3(0, 0, 10));
     camera.attachControl(canvas);
 
     var ground = new BABYLON.Mesh.CreateGround("ground", 50, 50, 0, scene);
-
 
     // Create light
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 10, 0), scene);
@@ -46,7 +76,23 @@ function initScene() {
 
     engine.runRenderLoop(function () {
         scene.render();
-        player.position.x = camera.position.x;
+        shots.forEach(function (shot, index) {
+            if (shot.position.z > 35) {
+                shot.dispose();
+                shots.splice(index, 1);
+            } else {
+                shot.position.z += .2;
+            }
+        })
+        if (keys.left === 1 && player.position.x > -22) {
+            player.position.x -= .2;
+        }
+        if (keys.right === 1 && player.position.x < 22) {
+            player.position.x += .2;
+        }
+        if (keys.space === 1 && shots.length < 3) {
+                shoot();
+        }
     })
 }
 
@@ -57,6 +103,7 @@ function initGame() {
 
     player = BABYLON.Mesh.CreateBox("player", { width: 2, height: 1, depth: 2}, scene, true);
     player.position.y = 1;
+    player.position.z = -20;
     camera.setTarget(scene.getMeshByName("player").position);
 
     var playerMaterial = new BABYLON.StandardMaterial("playerMat", scene);
@@ -64,5 +111,9 @@ function initGame() {
 
     player.setMaterialByID("playerMat");
 
+}
 
+function shoot() {
+    //Get the player position and create a line from the centre of it and move in z+
+    shots.push(new BABYLON.Mesh.CreateLines("shot", [new BABYLON.Vector3(player.position.x, .5, -19), new BABYLON.Vector3(player.position.x, .5, -18)], scene, true));
 }
