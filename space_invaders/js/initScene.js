a92400ad30f59a8b6b0c907643647fbf49e07927
John Bradshaw <john.bradshaw@senior.co.uk>
2015-10-07 09:08:23 +0100

    fixed lean juddering

--- space_invaders/js/initScene.js
+++ space_invaders/js/initScene.js
@@ -9,17 +9,22 @@ var ENDINGS = [];
 var keys = { left: 0, right: 0, space: 0 };
 var shot;
 var lastShotTime;
+
 var shots = [];
+var materialShot;
+
 var aliens = [];
 var alien;
 var alienCenter;
+var alienMat;
+var alienCount = 0;
+
 var playerVelocity = 0;
 var playerAcceleration = 0.025;
 var playerMaxSpeed = 0.3;
 var playerFriction = 0.025;
 var playerMaxLean = 45; //degrees 
 
-var alienCount = 0;
 var lastMoveTime = Date.now();
 var rowI = -20;
 var row = 0;
@@ -117,20 +122,6 @@ function initScene() {
 
             changed = false;
             moved = false;
-
-            //rows.forEach(function (row, indexR) {
-            //    //Get far left, see if it's past the left side
-            //    if (rows[indexR].aliens[0] !== undefined && rows[indexR].aliens[0].position.x) {
-            //        //It's past the left, start moving the other way
-            //        direction = 1;
-            //        changed = true;
-            //    }
-            //    if (rows[indexR].aliens[rows[indexR].aliens.length - 1] !== undefined && rows[indexR].aliens[rows[indexR].aliens.length - 1].position.x >= 22 && !changed) {
-            //        //It's past the right, start moving the other way
-            //        direction = -1;
-            //        ;
-            //    }
-            //});
             if (Date.now() - lastMoveTime > (20 * alienCount)) {
 
                 if ((alienCenter.x <= -22 && !changed) || (alienCenter.x >= 22 && !changed)) {
@@ -143,15 +134,7 @@ function initScene() {
                 moved = true;
             }
             rows.forEach(function (row, indexR) {
-
                 rows[indexR].aliens.forEach(function (alien, indexA) {
-                    //
-                    //    //TODO: Sort speed
-                    //    alien.position.x += 2 * direction;
-                    //    moved = true;
-                    //    if (changed)
-                    //        alien.position.z -= 2;
-                    //}
                     alien.position = alienCenter.add(alien.offsetPosition);
                     shots.forEach(function (shot, index) {
                         if (shot.line.intersectsMesh(alien, false)) {
@@ -171,14 +154,12 @@ function initScene() {
             playerVelocity *= 1-playerFriction;
 
             if (keys.left === 1 && player.position.x > -22) {
-                //player.position.x -= .2 * scene.getAnimationRatio();
                 if(playerVelocity > -playerMaxSpeed)
                 {
                     playerVelocity -= playerAcceleration;
                 }
             }
             if (keys.right === 1) {
-                //player.position.x += .2 * scene.getAnimationRatio();
                 if (playerVelocity < playerMaxSpeed) {
                     playerVelocity += playerAcceleration;
                 }
@@ -188,7 +169,6 @@ function initScene() {
             }
 
             player.position.x += playerVelocity * scene.getAnimationRatio();
-            //console.log(playerVelocity)
             if (player.position.x > 22)
             {
                 player.position.x = 22
@@ -199,7 +179,12 @@ function initScene() {
                 player.position.x = -22
                 playerVelocity = 0;
             }
-            player.rotation.x = BABYLON.Angle.FromDegrees(playerMaxLean * (playerVelocity/playerMaxSpeed)).radians()
+            
+            //all this does is stops it juddering when you hold a direction
+            var leanDegrees = (playerMaxLean*1.2) * (playerVelocity / playerMaxSpeed)
+            leanDegrees = playerVelocity > 0 ? Math.min(playerMaxLean, leanDegrees) : Math.max(-playerMaxLean, leanDegrees)
+            var lean = BABYLON.Angle.FromDegrees(leanDegrees).radians()   
+            player.rotation.x = lean;
         })
     }
 
@@ -211,22 +196,28 @@ function initScene() {
  */
 function initGame() {
 
-    //player = BABYLON.Mesh.CreateBox("player", { width: 2, height: 1, depth: 2}, scene, true);
     player.position.y = 1;
     player.position.z = -30;
     player.rotation.y = new BABYLON.Angle.FromDegrees(90).radians();
-    alienCenter = new BABYLON.Vector3(0, 1, 10);
+    
     camera.setTarget(scene.getMeshByName("space_frig").position);
 
     var playerMaterial = new BABYLON.StandardMaterial("playerMat", scene);
     playerMaterial.diffuseTexture = new BABYLON.Texture("./space_frigate_6_color.png", scene);
     playerMaterial.specularTexture = new BABYLON.Texture("./space_frigate_6_specular.png", scene);
     playerMaterial.emissiveTexture = new BABYLON.Texture("./space_frigate_6_illumination.png", scene);
+    player.material = playerMaterial
 
-    player.setMaterialByID("playerMat");
+    materialShot = new BABYLON.StandardMaterial("texture2", scene);
+    materialShot.diffuseColor = new BABYLON.Color3(1, 0, 0); //Red
+    materialShot.alpha = 0.8;
+    materialShot.emissiveColor = new BABYLON.Color3(1, 0, 0);
 
-    var alienMaterial = new BABYLON.StandardMaterial("alienMat", scene);
-    alienMaterial.diffuseColor = new BABYLON.Color3(1, 1, 0);
+
+    alienMat = new BABYLON.StandardMaterial("kosh", scene);
+    alienCenter = new BABYLON.Vector3(0, 1, 10);
+    
+
 
     //Create some aliens
     for (var i = 0; i < 54; i++) {
@@ -237,19 +228,24 @@ function initGame() {
 
 function shoot() {
     //Get the player position and create a line from the centre of it and move in z+
+
     if (shots.length > 0) {
         lastShotTime = shots[shots.length - 1].time;
         if (Date.now() - lastShotTime > 1000) {
             //If over a second has elapsed they can shoot again
-            shot = new BABYLON.Mesh.CreateLines("shot", [new BABYLON.Vector3(player.position.x, .5, -19), new BABYLON.Vector3(player.position.x, .5, -17.5)], scene, true);
-            shot.color = new BABYLON.Color3(24, 135, 216);
+            //shot = new BABYLON.Mesh.CreateLines("shot", [new BABYLON.Vector3(player.position.x, .5, -19), new BABYLON.Vector3(player.position.x, .5, -17.5)], scene, true);
+            shot = new BABYLON.Mesh.CreateSphere("shot", 5, 0.5, scene, true)
+            shot.position = new BABYLON.Vector3(player.position.x, .5, player.position.z+1)
+            shot.material = materialShot;
             shot.checkCollisions = true;
             shots.push({ line: shot, time: new Date() });
         }
     } else {
-        shot = new BABYLON.Mesh.CreateLines("shot", [new BABYLON.Vector3(player.position.x, .5, -19), new BABYLON.Vector3(player.position.x, .5, -17.5)], scene, true);
+        shot = shot = new BABYLON.Mesh.CreateSphere("shot",5, 0.5, scene, true)//new BABYLON.Mesh.CreateLines("shot", [new BABYLON.Vector3(player.position.x, .5, -19), new BABYLON.Vector3(player.position.x, .5, -17.5)], scene, true);
         shot.color = new BABYLON.Color3(24, 135, 216);
+        shot.position = new BABYLON.Vector3(player.position.x, .5, player.position.z+1)
         shot.checkCollisions = true;
+        shot.material = materialShot;
         shots.push({ line: shot, time: new Date() });
     }
 }
@@ -264,9 +260,9 @@ function createEnemy(i) {
     alien.position.y = 1;
     alien.position.x = rowI += 5;
     alien.position.z = -4 + (row * 3);
-    alien.setMaterialByID("alienMat");
     alien.checkCollisions = true;
     alien.offsetPosition = alienCenter.subtract(alien.position);
+    alien.material = alienMat;
     rows[row].aliens.push(alien);
     rowI++;
     alienCount++;
